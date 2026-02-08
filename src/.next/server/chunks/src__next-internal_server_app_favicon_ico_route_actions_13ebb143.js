b95d201a249b2179cf8ab15055bcc36e3de401e4
shabirkhan-dev <shabirkhan.dev@gmail.com>
2026-02-08 17:25:07 +0000

    feat: add TypeScript configuration and build tracing for improved development experience
    - Introduced tsconfig.json for TypeScript compiler options.
    - Added build tracing files to enhance performance monitoring.
    - Included necessary package.json files for build processes.
    
    This update aims to streamline the development workflow and improve build performance tracking.

--- src/.next/server/chunks/src__next-internal_server_app_favicon_ico_route_actions_13ebb143.js
+++
@@ -1,3 +0,0 @@
-module.exports=[13038,(e,o,d)=>{}];
-
-//# sourceMappingURL=src__next-internal_server_app_favicon_ico_route_actions_13ebb143.js.map
--- src/.next/server/chunks/ssr/src_app_page_tsx_a7111f3e._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[40777,a=>{"use strict";a.i(87924),a.i(72131);let b=Error("Cannot find module '@/agents/api-docs'");throw b.code="MODULE_NOT_FOUND",b}];
-
-//# sourceMappingURL=src_app_page_tsx_a7111f3e._.js.map
